76ddbfd9424d7dba2cb0dbbdeb6d351ea93d24ef
Valentino banyu ampar paraya <144188174+nyukkiw@users.noreply.github.com>
2025-08-12 18:18:27 +0700

    first app project

---
+++ app/layout.jsx
@@ -0,0 +1,15 @@
+export default function layout({children}){
+    return(
+        
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>My next.JS FUNDAMENTAL</title>
+        </head>
+        <body>
+            {children}
+        </body>
+        </html>
+    )
+}
