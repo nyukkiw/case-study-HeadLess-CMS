8953e437dae9f48de80ab395e361e8b7f7328e1c
Valentino banyu ampar paraya <144188174+nyukkiw@users.noreply.github.com>
2025-08-26 12:14:22 +0700

    author issue

--- scripts/strapi-request.mjs
+++ scripts/strapi-request.mjs
@@ -1,10 +1,22 @@
 import { writeFileSync } from "node:fs";
+import qs from 'qs';
 
-const url = "http://localhost:1337/api/posts" + "?populate=*";
+const url =
+"http://localhost:1337/api/posts" + "?" +  qs.stringify(
+    {
+      fields: ["slug", "title", "description", "body"],
+      populate: { 
+        image: {fields:["url"]},
+        // author: { fields: ["username", "email"] }
+    
+    }, 
+    },
+    { encodeValuesOnly: true }
+  );
 const response = await fetch(url);
 const body = await response.json();
 const posts = JSON.stringify(body, null, 2);
-console.log(posts);
+
 
 const file = 'scripts/strapi-response.json'
 writeFileSync(file, posts, 'utf8')
